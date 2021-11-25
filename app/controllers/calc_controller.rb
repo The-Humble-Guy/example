01bc28a731cba1323cfa0e4cb1879c0609904171
The-Humble-Guy <ftruf357ft@gmail.com>
2021-11-25 22:57:37 +0300

    Добавил в комит

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,15 @@
+class CalcController < ApplicationController
+  def input
+  end
+
+  def view
+    v1, v2 = params[:v1].to_i, params[:v2].to_i;
+    @result = case params[:op]
+              when "+" then v1+v2;
+              when "-" then v1-v2;
+              when "*" then v1*v2;
+              when "/" then v1/v2;
+              else "Unknown!"
+              end
+  end
+end
